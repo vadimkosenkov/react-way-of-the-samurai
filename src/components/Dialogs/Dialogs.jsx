426d62542dbe3c9483dede71b7b3d2324f6f0787
Vadim Kosenkov <vadimkosenkov.work@gmail.com>
2021-06-27 19:18:23 +0300

    add Dialogs.jsx/module,css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 import './../../App.css';
 
 const Dialogs = () => {
-  return <div className={'app-wrapper_content'}>Dialogs</div>;
+  return (
+    <div className={'app-wrapper_content'}>
+      <div className={s.container}>
+        <div className={s.dialog_items}>
+          <div className={s.item}>
+            <NavLink to='/dialogs/Mike' activeClassName={s.activeLink}>
+              Майк Дельфино
+            </NavLink>
+          </div>
+          <div className={s.item}>
+            <NavLink to='/dialogs/Carlos' activeClassName={s.activeLink}>
+              Карлос Солис
+            </NavLink>
+          </div>
+          <div className={s.item}>
+            <NavLink to='/dialogs/Tom' activeClassName={s.activeLink}>
+              Том Скаво
+            </NavLink>
+          </div>
+        </div>
+        <div className={s.message_items}>
+          <div className={s.item}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, id nostrum!</div>
+          <div className={s.item}>Dolor incidunt expedita est eveniet soluta.</div>
+          <div className={s.item}>
+            Ab nostrum deserunt, provident obcaecati beatae laborum cum minima unde officia consectetur!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
